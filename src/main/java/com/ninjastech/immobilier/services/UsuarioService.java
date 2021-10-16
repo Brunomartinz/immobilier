71324e52c92499ab2578574cd6486c2496278ffe
unknown <wesley132013@gmail.com>
2021-10-16 00:23:14 -0300

    criação do alterar usuario e inativar usuario, com validação de senha e criptografia

--- src/main/java/com/ninjastech/immobilier/services/UsuarioService.java
+++ src/main/java/com/ninjastech/immobilier/services/UsuarioService.java
@@ -1,6 +1,7 @@
 package com.ninjastech.immobilier.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -32,6 +33,12 @@ public class UsuarioService {
 	return usuarioRepository.findByEmail(email);
 }
 
+    // Busca o produto pelo Id
+    public Usuario findById(Long id) {
+        Optional<Usuario> obj = usuarioRepository.findById(id);
+        return obj.get();
+    }
+
     // Insere no banco de dados um novo objeto do tipo Usuário
     public Usuario insertUsuario(Usuario obj) {
         return usuarioRepository.save(obj);
@@ -41,9 +48,11 @@ public class UsuarioService {
     //Deleção de usuário por id
     public void deletUser(Usuario obj){
         usuarioRepository.deleteById(obj.getId());
-        }
+    }
+
     //Alteração Users
     public Usuario editUsuario(Usuario obj){
+        System.out.println("senhaaaaaaaaaaaaaaaaaaaa"+obj.getSenha());
         return usuarioRepository.save(obj);
 }
     
