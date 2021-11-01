91e80671a67b22d15bcc107f2a5afd49d6fe1de0
unknown <wesley132013@gmail.com>
2021-11-01 15:32:02 -0300

    alterações na usabilidade e layout do crud de cliente/endereço

--- src/main/java/com/ninjastech/immobilier/services/ClienteEnderecoService.java
+++ src/main/java/com/ninjastech/immobilier/services/ClienteEnderecoService.java
@@ -3,7 +3,6 @@ package com.ninjastech.immobilier.services;
 import com.ninjastech.immobilier.entities.ClienteEndereco;
 import com.ninjastech.immobilier.repositories.ClienteEnderecoRepository;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -27,11 +26,11 @@ public class ClienteEnderecoService {
     }
 
     public ClienteEndereco insertEndereco(ClienteEndereco obj) {
+        obj.setPrincipal(false);
         return clienteEnderecoRepository.save(obj);
-        
-        
     }
+    
     public ClienteEndereco editCliente(ClienteEndereco obj){
-                return clienteEnderecoRepository.save(obj);
-        }
+        return clienteEnderecoRepository.save(obj);
+    }
 }
