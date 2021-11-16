30609912fbdcda0ad668c4025bdc6b098dee392b
Arnaldo <arnaldoalves34@gmail.com>
2021-11-15 22:20:37 -0300

    Carrinho Apaga apos finalizar pedido

--- src/main/resources/static/DataTables/js/index.js
+++ src/main/resources/static/DataTables/js/index.js
@@ -47,9 +47,7 @@ window.onload = function () {
 	}
 	// adding data to shopping cart 
 	let qtd = 0;
-	JSON.parse(localStorage.getItem('produtos')).map(data => {
-		qtd = qtd + data.qtd;
-	});
+	
 
 	//adding cartbox data in table
 	const cardBoxTable = document.querySelector('#div-produtos');
@@ -59,9 +57,18 @@ window.onload = function () {
 	let spanSubtotal = document.getElementById('subtotal')
 	let totalGeral = 0
 	if (JSON.parse(localStorage.getItem('produtos')) === null) {
+		console.log("vazio")
 		tableData += '<div>Você não tem produtos no carrinho</div>'
 	} else {
 		JSON.parse(localStorage.getItem('produtos')).map(data => {
+
+			JSON.parse(localStorage.getItem('produtos')).map(data => {
+				qtd = qtd + data.qtd;
+				if(qtd == 0){
+					
+					
+				}
+			});
 			let total = Number(data.qtd) * Number(data.preco)
 			totalGeral += Number(data.qtd) * Number(data.preco)
 			tableData += '<div class="flex items-center justify-between hover:bg-gray-100 -mx-8 px-6 py-5"><span class="hidden">' + data.id + '</span><div class="hidden"><span class="hidden">' + data.nome + '</span></div><div class="flex w-2/5"><div class="w-20"><img class="h-24" src="'+data.imgUrl+'" alt=""><div></div></div><div class="flex flex-col justify-between ml-4 flex-grow"><span class="font-bold text-sm">' + data.nome + '</span><span class="text-red-500 text-xs">' + data.avaliacao + '</span><a onclick="Delete(this)" class="cursor-pointer font-semibold hover:text-red-500 text-gray-500 text-xs">Remove</a></div></div><div class="flex justify-center w-1/5"><span onclick="reduzir(this)" class="cursor-pointer mt-1.5"><svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512"><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" /></svg></span><input class="mx-2 border text-center w-8" type="text" value="' + data.qtd + '"><span onclick="aumentar(this)" class="cursor-pointer mt-1.5"><svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512"><path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" /></svg></span></div><span class="text-center w-1/5 font-semibold text-sm hidden lg:inline-block">' + data.preco + '</span><span class="text-center w-1/5 font-semibold text-sm">' + total + '</span></div>'
